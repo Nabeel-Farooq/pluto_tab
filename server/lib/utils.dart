91565d784edc489574736dea6fa16a2400bceb1a
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-05-25 14:02:11 +0500

    Update utils.dart

--- server/lib/utils.dart
+++ server/lib/utils.dart
@@ -5,16 +5,26 @@ import 'package:http_interceptor/http_interceptor.dart';
 import 'package:screwdriver/screwdriver.dart';
 
 class LoggerInterceptor extends InterceptorContract {
+  static const String _separator =
+      '--------------------------------------------------------------------------------';
+
   @override
   Future<BaseRequest> interceptRequest({
     required BaseRequest request,
   }) async {
-    print('-' * 80);
-    print('REQUEST: $request');
-    print('-' * 80);
-    print('HEADERS:');
-    print(request.headers.toString());
-    print('-' * 80);
+    _printSeparator();
+    print('REQUEST: ${request.method} ${request.url}');
+    _printSeparator();
+
+    if (request.headers.isNotEmpty) {
+      print('HEADERS:');
+      _printHeaders(request.headers);
+    } else {
+      print('HEADERS: <empty>');
+    }
+
+    _printSeparator();
+
     return request;
   }
 
@@ -22,20 +32,39 @@ class LoggerInterceptor extends InterceptorContract {
   Future<BaseResponse> interceptResponse({
     required BaseResponse response,
   }) async {
-    print('-' * 80);
-    print('RESPONSE: ${response.statusCode} ${response.request}');
-    print('-' * 80);
-    print('HEADERS:');
-    final maxKeyLength = response.headers.keys.map((e) => e.length).max;
-    response.headers.entries
-        .sorted((a, b) => a.key.compareTo(b.key))
-        .forEach((entry) => print('${entry.key.padRight(maxKeyLength)} : ${entry.value}'));
+    _printSeparator();
+    print(
+      'RESPONSE: ${response.statusCode} '
+      '${response.request?.method} '
+      '${response.request?.url}',
+    );
+    _printSeparator();
+
+    if (response.headers.isNotEmpty) {
+      print('HEADERS:');
+      _printHeaders(response.headers);
+    } else {
+      print('HEADERS: <empty>');
+    }
+
     if (response case Response(:final body)) {
+      _printSeparator();
       print('BODY:');
-      if (response.headers[HttpHeaders.contentTypeHeader]?.contains('application/json') == true) {
+
+      final contentType =
+          response.headers[HttpHeaders.contentTypeHeader];
+
+      if (_isJson(contentType)) {
         final json = tryJsonDecode(body);
+
         if (json != null) {
-          print(const JsonEncoder.withIndent('  ').convert(json));
+          try {
+            print(
+              const JsonEncoder.withIndent('  ').convert(json),
+            );
+          } catch (_) {
+            print(body);
+          }
         } else {
           print(body);
         }
@@ -43,7 +72,40 @@ class LoggerInterceptor extends InterceptorContract {
         print(body);
       }
     }
-    print('-' * 80);
+
+    _printSeparator();
+
     return response;
+  }
+
+  void _printSeparator() {
+    print(_separator);
+  }
+
+  void _printHeaders(Map<String, String> headers) {
+    if (headers.isEmpty) {
+      print('<empty>');
+      return;
+    }
+
+    final maxKeyLength = headers.keys
+        .map((e) => e.length)
+        .fold<int>(0, (prev, curr) => curr > prev ? curr : prev);
+
+    headers.entries
+        .sorted((a, b) => a.key.compareTo(b.key))
+        .forEach(
+          (entry) => print(
+            '${entry.key.padRight(maxKeyLength)} : ${entry.value}',
+          ),
+        );
+  }
+
+  bool _isJson(String? contentType) {
+    if (contentType == null) {
+      return false;
+    }
+
+    return contentType.contains('application/json');
   }
 }
